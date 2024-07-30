a8445f15d05466c629c41954ba113f83b8d127db
hannah-no <21425@apps.eggs.school.nz>
2024-07-30 13:54:07 +1200

    Updating Component

--- countries&infected_component.py
+++ countries&infected_component.py
@@ -1,46 +1,46 @@
 # Insures number of infected is an integer between 0-200
-def validate_num_infected(min_amount, max_amount):
-      """Purpose: Validates the input of the number of infected
-      Passed: An integer indicating the max number of infected
-      Returns: A positive integer between 0 and 200"""
+def validate_num_infected(min_amount):
+    """Purpose: Validates the input of the number of infected
+    Passed: An integer indicating the max number of infected
+    Returns: A positive integer between 0 and 200"""
 
-      while True:
-            try:
-                  input_data = int(input())
-                  if input_data >= min_amount and input_data <= max_amount:
-                        return input_data
-                  else:
-                        print("The number of infected mus be between", min_amount, "and", max_amount)
-            except:
-                  print("The number of infected must be a whole number between", min_amount, "and", max_amount)
+    while True:
+        try:
+            input_data = int(input())
+            if input_data >= min_amount and input_data <= max_num_infected:
+                return input_data
+            else:
+                print("The number of infected must be between", min_amount,  "and", max_num_infected, ":")
+        except:
+            print("The number of infected must be a whole number between", min_amount,  "and", max_num_infected, ":")
 
 
 # Data Creation: Prompt users to create and give the number of infected for 3 countries
-def create_country(countries, infected):
-      """Purpose: Prompts user to input countries and number of infected to be stored
-      Passed: countries and infected
-      Returns: Nothing"""
+def create_country():
+    """Purpose: Prompts user to input countries and number of infected to be stored
+    Passed: countries and infected
+    Returns: Nothing"""
 
-      MIN_AMOUNT = 50
-      MAX_AMOUNT = 200
+    MIN_AMOUNT = 50
 
-      while len(countries) != 3:
-            print("Please enter the name of your country:")
+    while len(countries) != 3:
+        print("Please enter the name of your country:")
+        country_name = input()
+
+        while len(country_name) == 0:
+            print("You didn't enter anything. Please try again:")
             country_name = input()
 
-            while len(country_name) == 0:
-                  print("You didn't enter anything. Please try again:")
-                  country_name = input()
-
-            countries.append(country_name)
-            print("Please enter the number of infected in this country. "
-                  "The number of infected mus be between", MIN_AMOUNT, "and", MAX_AMOUNT, ":")
-            num_infected = validate_num_infected(MIN_AMOUNT, MAX_AMOUNT)
-            infected.append(num_infected)
+        countries.append(country_name)
+        print("Please enter the number of infected in this country.")
+        print("The number of infected must be between", MIN_AMOUNT, "and", max_num_infected, ":")
+        num_infected = validate_num_infected(MIN_AMOUNT)
+        infected.append(num_infected)
 
 
 countries = []
 infected = []
+max_num_infected = 200
 create_country(countries, infected)
 print(countries)
 print(infected)
